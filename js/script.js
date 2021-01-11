7dfbc5cb2ca803621ad68d3d46bcd0ffd0c7850f
epioletti <PiolettiElizabethL@JohnDeere.com>
2021-01-11 08:23:06 -0800

    created js file

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("In javascript")
+arrayNumbers=[0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7]
+
+
+
+
+
+
+function displayNumber(evt){
+    evt.preventDefault()
+ 
+}
+
+
+
+let sq=document.querySelector(`#${CSS.escape("0")}`);
+sq.addEventListener("click", displayNumber)
+// .addEventListener("click", displayNumber())
+console.log(sq)
+
