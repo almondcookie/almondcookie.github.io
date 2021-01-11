380e7b02ffd9644b4ec99a230d57934e8c4e52b4
epioletti <PiolettiElizabethL@JohnDeere.com>
2021-01-11 08:28:26 -0800

    added reset button

--- js/script.js
+++ js/script.js
@@ -15,5 +15,52 @@ function displayNumber(evt){
 let sq=document.querySelector(`#${CSS.escape("0")}`);
 sq.addEventListener("click", displayNumber)
 // .addEventListener("click", displayNumber())
-console.log(sq)
+//console.log(sq)
+
+let sq1 = document.querySelector(`#${CSS.escape("1")}`);
+sq1.addEventListener("click",displayNumber)
+
+let sq2 = document.querySelector(`#${CSS.escape("2")}`);
+sq2.addEventListener("click",displayNumber)
+
+let sq3 = document.querySelector(`#${CSS.escape("3")}`);
+sq3.addEventListener("click",displayNumber)
+
+let sq4 = document.querySelector(`#${CSS.escape("4")}`);
+sq4.addEventListener("click",displayNumber)
+
+let sq5 = document.querySelector(`#${CSS.escape("5")}`);
+sq5.addEventListener("click",displayNumber)
+
+let sq6 = document.querySelector(`#${CSS.escape("6")}`);
+sq6.addEventListener("click",displayNumber)
+
+let sq7 = document.querySelector(`#${CSS.escape("7")}`);
+sq7.addEventListener("click",displayNumber)
+
+let sq8 = document.querySelector(`#${CSS.escape("8")}`);
+sq8.addEventListener("click",displayNumber)
+
+let sq9 = document.querySelector(`#${CSS.escape("9")}`);
+sq9.addEventListener("click",displayNumber)
+
+let sq10 = document.querySelector(`#${CSS.escape("10")}`);
+sq10.addEventListener("click",displayNumber)
+
+let sq11 = document.querySelector(`#${CSS.escape("11")}`);
+sq11.addEventListener("click",displayNumber)
+
+let sq12 = document.querySelector(`#${CSS.escape("12")}`);
+sq12.addEventListener("click",displayNumber)
+
+let sq13 = document.querySelector(`#${CSS.escape("13")}`);
+sq13.addEventListener("click",displayNumber)
+
+let sq14 = document.querySelector(`#${CSS.escape("14")}`);
+sq14.addEventListener("click",displayNumber)
+
+let sq15 = document.querySelector(`#${CSS.escape("15")}`);
+sq15.addEventListener("click",displayNumber)
+
+button=document.querySelector("button")
 
