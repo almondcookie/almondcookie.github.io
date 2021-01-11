c40684e192378c625ab20e9c1e9b8dc588e4644d
epioletti <PiolettiElizabethL@JohnDeere.com>
2021-01-11 08:37:39 -0800

    display preset numbers on the gameboard

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ arrayNumbers=[0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7]
 
 function displayNumber(evt){
     evt.preventDefault()
+    const currentIndex = evt.target.id
+    const currentNumber = arrayNumbers[currentIndex]
+    evt.target.innerText = currentNumber
+
  
 }
 
