0d3816acbb96ace5a8c4a0645e7d63b3cb55c680
epioletti <PiolettiElizabethL@JohnDeere.com>
2021-01-11 08:43:12 -0800

    Implemented if there is a match or not

--- js/script.js
+++ js/script.js
@@ -1,18 +1,61 @@
 console.log("In javascript")
 arrayNumbers=[0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7]
+firstSquarePicked=[99,99]
+squaresMatched=[]
 
 
-
-
+// Got sleep function from https://www.sitepoint.com/delay-sleep-pause-wait/
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
 
 
 function displayNumber(evt){
     evt.preventDefault()
+    //console.log(evt)
+    //console.log(evt.target.id)
     const currentIndex = evt.target.id
     const currentNumber = arrayNumbers[currentIndex]
-    evt.target.innerText = currentNumber
+    const previousIndex = firstSquarePicked[0]
+    const previousNumber = firstSquarePicked[1]
+    evt.target.innerText= currentNumber
+    // Check for a match.
+    // Check if a previous cell has been checked
+    // If not, put location and value of the cell
+    // Then next time around, can compare firstSquarePicked to the current value
 
- 
+    // Check if the square has already found a match
+    if (!squaresMatched.includes(currentIndex)){
+        // starting with comparisons again
+        if (firstSquarePicked[0] == 99){
+            firstSquarePicked[0] = evt.target.id  //Store which square
+            firstSquarePicked[1] = currentNumber //Store the number
+        // CHeck if no match   
+        } else if (previousNumber !== currentNumber){
+                // const previousNumber = document.querySelector('#' + CSS.escape(previousIndex)).innerText
+                // console.log("previousNumber: " + previousNumber)            
+                console.log(`no match. ${currentNumber} Not Equal ${previousNumber}. Previous location: ${previousIndex}`)
+                //sleep(2000)
+                // Clear previous text from gameboard
+                document.querySelector('#' + CSS.escape(previousIndex)).innerText = ""
+                // Clear current text from gameboard
+                evt.target.innerText = ""
+                firstSquarePicked[0]=99
+                firstSquarePicked[1]=99
+            // check for match
+        } else if(previousNumber == currentNumber){
+            console.log("Match current and previous " + currentNumber + " " + previousNumber)
+            squaresMatched[currentIndex]=currentIndex
+            squaresMatched[previousIndex]=previousIndex
+            firstSquarePicked[0]=99
+            firstSquarePicked[1]=99
+
+        }
+    }
 }
 
 
@@ -65,6 +108,4 @@ sq14.addEventListener("click",displayNumber)
 
 let sq15 = document.querySelector(`#${CSS.escape("15")}`);
 sq15.addEventListener("click",displayNumber)
-
-button=document.querySelector("button")
 
